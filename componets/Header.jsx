dd439a966347f301bb763f637c81a1671e18489f
Shubam-08 <shubamgupta165@gmail.com>
2025-04-07 02:23:56 +0530

    bloglist category wise added

--- componets/Header.jsx
+++ componets/Header.jsx
@@ -12,6 +12,10 @@ const Header = () => {
       <div className='text-center my-8'>
         <h1 className='text-3xl sm:text-5xl font-medium'>Latest BLogs</h1>
         <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur maxime officiis similique repellat aliquid neque quos sed. Soluta, dicta animi. Quibusdam minus eum recusandae vero neque sint tempore quos eligendi.</p>
+        <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' >
+          <input type="email" placeholder='Enter your email' className='pl-4 outline-none'/>
+          <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white '>Subscribe</button>
+        </form>
       </div>
     </div>
   )
